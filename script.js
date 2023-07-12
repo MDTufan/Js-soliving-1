86e8e84afe400b5c1aacba54d0782d8f73bbd156
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-07-12 08:16:15 -0700

    Add files via upload
    soliving

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+// function smlNumber( NumberOfArray){
+       
+//    let smlnum = NumberOfArray[0];
+//    for(let i = 0; i<NumberOfArray.length;i++){
+//         if (NumberOfArray[i]<smlnum){
+//                 smlnum = NumberOfArray[i];
+//         }
+//    }     
+//  return smlnum;
+// }
+
+
+// let result = smlNumber([50,12,62,18,43,9,102]);
+// console.log(result);
+
+
+
+// const names = ["toli","tufan","janatun","islam","amawara","toli","tufan","janatun","islam","amawara","toli","tufan","janatun","islam","amawara","toli","tufan","janatun","islam","amawara"];
+
+
+// function removepople(array){
+//         const uniqueName = [];
+//         for(let i = 0; i<array.length; i++) {
+//               if(uniqueName.indexOf(names[i]) == -1){
+//                 uniqueName.push(names[i]);
+//               }
+                
+//         }
+//         return uniqueName;
+// }
+
+// let result =  removepople(names);
+// console.log(result);
+
+// let Name = "Salman Khan";
+// function rename(sentence){
+//     let rever = "";
+//     for(let i=0; i<sentence.length; i++){
+//         let letter = sentence[i];
+//         rever = letter + rever;
+       
+//     }
+
+//     return reversed;
+// }
+
+// console.log(rename(Name));
+
+// const Item = [
+//         {name: "boys", price: 520, color: "black", qty: 2},     
+//         {name: "girls", price: 620, color: "blue", qty: 5},
+//         {name: "men", price: 220, color: "brown", qty: 3},
+//         {name: "women", price: 1020, color: "pink", qty: 4},
+//         {name: "children", price: 120, color: "yellow", qty: 6},
+// ];
+
+// let totalShopping = 0;
+
+//  for(const item of Item){
+//         // console.log(item);
+//         // console.log(item.price);
+//         // console.log(item.qty);
+//          const singleItemTotalPrice = item.price * item.qty;
+//          totalShopping += singleItemTotalPrice
+// }
+
+// console.log(totalShopping)
